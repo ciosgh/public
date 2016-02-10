1a43e3e9a9ac042f947cdb924aa41b83051ef5bc
Darren <darren@ciosgrowthhub.com>
2016-02-10 15:56:39 +0000

    commit
    commit

--- wp-content/themes/x/index.php
+++ wp-content/themes/x/index.php
@@ -1,38 +1,18 @@
 <?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme and one of the
- * two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * For example, it puts together the home page when no home.php file exists.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package WordPress
- * @subpackage Twenty_Thirteen
- * @since Twenty Thirteen 1.0
- */
 
-get_header(); ?>
+// =============================================================================
+// INDEX.PHP
+// -----------------------------------------------------------------------------
+// Handles output of pages and posts if a more specific template file isn't
+// present. Must be present for theme to function properly.
+//
+// Content is output based on which Stack has been selected in the Customizer.
+// To view and/or edit the markup of your Stack's index, first go to "views"
+// inside the "framework" subdirectory. Once inside, find your Stack's folder
+// and look for a file called "wp-index.php," where you'll be able to find the
+// appropriate output.
+// =============================================================================
 
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
-		<?php if ( have_posts() ) : ?>
+?>
 
-			<?php /* The loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', get_post_format() ); ?>
-			<?php endwhile; ?>
-
-			<?php twentythirteen_paging_nav(); ?>
-
-		<?php else : ?>
-			<?php get_template_part( 'content', 'none' ); ?>
-		<?php endif; ?>
-
-		</div><!-- #content -->
-	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php x_get_view( x_get_stack(), 'wp', 'index' ); ?>
