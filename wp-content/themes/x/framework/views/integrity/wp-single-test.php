3f47dff1fc9a6316f03aab7d3f15121a833b8193
Aaron <aaron@ciosgrowthhub.com>
2016-02-12 10:27:12 +0000

    quick sync test - AC
    Just added the word another

--- wp-content/themes/x/framework/views/integrity/wp-single-test.php
+++ wp-content/themes/x/framework/views/integrity/wp-single-test.php
@@ -14,7 +14,7 @@ $fullwidth = get_post_meta( get_the_ID(), '_x_post_layout', true );
 
   <div class="x-container max width offset">
     <div class="<?php x_main_content_class(); ?>" role="main">
-Hello this is a test post
+Hello this is another test post
       <?php while ( have_posts() ) : the_post(); ?>
         <?php x_get_view( 'integrity', 'content', get_post_format() ); ?>
         <?php x_get_view( 'global', '_comments-template' ); ?>
